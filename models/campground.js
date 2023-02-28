10a382b065d1a5ed3d29bd35669879d7e20c3b1f
Dev <dev200310@gmail.com>
2023-02-27 23:25:44 -0500

    Added basic create, read, update, and delete (CRUD) functionality for campgrounds

---
+++ models/campground.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const CampgroundSchema = new mongoose.Schema({
+    title: String,
+    price: String,
+    description: String,
+    location: String
+})
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
---
+++ seeds/index.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const {places, descriptors} = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async function() {
+    await Campground.deleteMany({});
+
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const newCampground = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await newCampground.save();
+    }
+};
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
