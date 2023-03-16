8f37dc5f1bd0e3e367186745ec3370810f84aff0
Dev <dev200310@gmail.com>
2023-03-16 14:53:42 -0400

    embedded dotenv variables

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
 const MongoDBStore = require('connect-mongo')(session);
 
 mongoose.set('strictQuery', true);
